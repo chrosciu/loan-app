5fb6d5714f10189f5ed761e908ba923483d3a6d7
Marcin Chrost <mchrost82@gmail.com>
2023-10-23 17:45:54 +0200

    Modularization

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+open module eu.chrost.loan {
+    requires static lombok;
+    requires spring.web;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+    requires spring.beans;
+}
