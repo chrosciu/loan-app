cdf5f5f0bd755c05b3747f41aff5e4cd826b8fa0
Marcin Chrost <mchrost82@gmail.com>
2023-12-19 09:50:43 +0100

    Replaced immutable classes with records

--- src/main/java/eu/chrost/loan/Suspension.java
+++ src/main/java/eu/chrost/loan/Suspension.java
@@ -1,12 +1,6 @@
 package eu.chrost.loan;
 
-import lombok.Value;
-
 import java.time.LocalDate;
 import java.util.List;
 
-@Value
-public class Suspension implements Result {
-    List<String> additionalRequirements;
-    LocalDate deadline;
-}
+public record Suspension(List<String> additionalRequirements, LocalDate deadline) implements Result {}
