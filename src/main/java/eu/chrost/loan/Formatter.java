4731157a18c91f8887c4e5fbd3ed9765c258bb46
Marcin Chrost <mchrost82@gmail.com>
2023-12-19 11:10:24 +0100

    Building message for suspension extracted to separate method

--- src/main/java/eu/chrost/loan/Formatter.java
+++ src/main/java/eu/chrost/loan/Formatter.java
@@ -17,18 +17,20 @@ public class Formatter {
             case Approval approval when approval.amount() >= request.amount() -> "Loan approved, granted full amount";
             case Approval approval -> String.format("Loan approved, amount granted: %d", approval.amount());
             case Refusal refusal -> String.format("Loan refused due to: %s", refusal.reason());
-            case Suspension suspension -> {
-                var builder = new StringBuilder("""
-                        Loan processing suspended.
-                        Following additional requirements are needed to make final decision:
-                        """);
-                for (var requirement : suspension.additionalRequirements()) {
-                    builder.append(requirement);
-                    builder.append("\n");
-                }
-                builder.append(String.format("Deadline to fulfill requirements mentioned above: %s", suspension.deadline()));
-                yield builder.toString();
-            }
+            case Suspension suspension -> buildMessageForSuspension(suspension);
         };
+    }
+
+    private String buildMessageForSuspension(Suspension suspension) {
+        var builder = new StringBuilder("""
+                Loan processing suspended.
+                Following additional requirements are needed to make final decision:
+                """);
+        for (var requirement : suspension.additionalRequirements()) {
+            builder.append(requirement);
+            builder.append("\n");
+        }
+        builder.append(String.format("Deadline to fulfill requirements mentioned above: %s", suspension.deadline()));
+        return builder.toString();
     }
 }
