458b9e2edc42582727c93337336f5b08a9f63393
Marcin Chrost <mchrost82@gmail.com>
2023-12-19 10:46:55 +0100

    Replaced if...else with pattern matching for switch

--- src/main/java/eu/chrost/loan/Formatter.java
+++ src/main/java/eu/chrost/loan/Formatter.java
@@ -13,25 +13,22 @@ public class Formatter {
     }
 
     private String prepareMessage(Request request, Result result) {
-        if (result instanceof Approval approval) {
-            var amount = approval.amount();
-            if (amount >= request.amount()) {
-                return "Loan approved, granted full amount";
+        return switch (result) {
+            case Approval approval when approval.amount() >= request.amount() -> "Loan approved, granted full amount";
+            case Approval approval -> String.format("Loan approved, amount granted: %d", approval.amount());
+            case Refusal refusal -> String.format("Loan refused due to: %s", refusal.reason());
+            case Suspension suspension -> {
+                var builder = new StringBuilder();
+                builder.append("Loan processing suspended.\n");
+                builder.append("Following additional requirements are needed to make final decision:\n");
+                for (var requirement : suspension.additionalRequirements()) {
+                    builder.append(requirement);
+                    builder.append("\n");
+                }
+                builder.append(String.format("Deadline to fulfill requirements mentioned above: %s", suspension.deadline()));
+                yield builder.toString();
             }
-            return String.format("Loan approved, amount granted: %d", amount);
-        } else if (result instanceof Refusal refusal) {
-            return String.format("Loan refused due to: %s", refusal.reason());
-        } else if (result instanceof Suspension suspension) {
-            var builder = new StringBuilder();
-            builder.append("Loan processing suspended.\n");
-            builder.append("Following additional requirements are needed to make final decision:\n");
-            for (var requirement : suspension.additionalRequirements()) {
-                builder.append(requirement);
-                builder.append("\n");
-            }
-            builder.append(String.format("Deadline to fulfill requirements mentioned above: %s", suspension.deadline()));
-            return builder.toString();
-        }
-        throw new IllegalStateException("Unknown result type");
+            default -> throw new IllegalStateException("Unknown result type");
+        };
     }
 }
