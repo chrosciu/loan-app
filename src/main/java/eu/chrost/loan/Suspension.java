b464409b486e8a038a7c4f38566e6e829971e6dd
Marcin Chrost <mchrost82@gmail.com>
2024-02-23 16:41:39 +0100

    Changed canonica constructor in Suspension record to protect against mutable component

--- src/main/java/eu/chrost/loan/Suspension.java
+++ src/main/java/eu/chrost/loan/Suspension.java
@@ -3,4 +3,9 @@ package eu.chrost.loan;
 import java.time.LocalDate;
 import java.util.List;
 
-public record Suspension(List<String> additionalRequirements, LocalDate deadline) implements Result {}
+public record Suspension(List<String> additionalRequirements, LocalDate deadline) implements Result {
+    public Suspension(List<String> additionalRequirements, LocalDate deadline) {
+        this.additionalRequirements = List.copyOf(additionalRequirements);
+        this.deadline = deadline;
+    }
+}
